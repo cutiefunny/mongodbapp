aa2e22cc2cf9e3c6d98fc019c0e3eaaee9131a21
cutiefunny <cutiefunny@gmail.com>
2024-01-22 12:44:51 +0900

    new

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,9 @@
+import { HiOutlineTrash } from "react-icons/hi"
+
+export default function RemoveBtn(){
+    return (
+        <button style={{ color: "red" }}>
+            <HiOutlineTrash size={24} />
+        </button>
+    )
+}
